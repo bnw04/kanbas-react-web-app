705070bfd473dedfee260e59ee886f796bf114d6
Beini W <wang.beini@northeastern.edu>
2024-03-19 17:29:34 -0400

    a4 State and Redux fa23

--- src/Kanbas/Courses/Assignments/dateFormat.tsx
+++ src/Kanbas/Courses/Assignments/dateFormat.tsx
@@ -1,5 +1,6 @@
 function formatDate(dateStr: string) {
-  const date = new Date(dateStr);
+  // Create a date object assuming dateStr is in UTC
+  const date = new Date(dateStr + 'T00:00:00Z');
   const options: Intl.DateTimeFormatOptions = { month: 'short', day: '2-digit' };
   const formattedDate = date.toLocaleDateString('en-US', options);
   
